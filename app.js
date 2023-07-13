59f1bca211b101802a40cc1732951ab6eeb0cc6b
Jatin Takkar <jatintakkar@ymail.com>
2023-07-13 19:45:24 +0530

    added batsman runs, extras + bye/leg bye logic

--- app.js
+++ app.js
@@ -5,51 +5,56 @@ const app = createApp({
     return {
       moduleNo: 1,
       moduleName: 'Confirm your playing 11',
-      playingXi: ['Cook', 'Strauss', 'Trott', 'Bell', 'Pietersen', 'Collingwood', 'Prior', 'Swann', 'Broad', 'Anderson', 'Finn'],
+      // playingXi: ['Cook', 'Strauss', 'Trott', 'Bell', 'Pietersen', 'Collingwood', 'Prior', 'Swann', 'Broad', 'Anderson', 'Finn'],
+      playingXi: [{ name: 'Cook', runs: 0, balls: 0 }, { name: 'Strauss', runs: 0, balls: 0 }, { name: 'Trott', runs: 0, balls: 0 }, { name: 'Bell', runs: 0, balls: 0 }, { name: 'Pietersen', runs: 0, balls: 0 }, { name: 'Collingwood', runs: 0, balls: 0 }, { name: 'Prior', runs: 0, balls: 0 }, { name: 'Swann', runs: 0, balls: 0 }, { name: 'Broad', runs: 0, balls: 0 }, { name: 'Anderson', runs: 0, balls: 0 }, { name: 'Finn', runs: 0, balls: 0 }],
       cpxi: true,
       csxi: false,
-      scoringOptions: { 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', n: 'NB', w: 'WB', l: 'LB', o: 'OUT' },
+      scoringOptions: { 0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', n: 'NB', w: 'WB', l: 'LB', b: 'B', o: 'OUT' },
       score: 0,
       wickets: 0,
       balls: 0,
       overs: 0.0,
       cont: true,
       strike: '*',
-      activeBatsman: 1
+      activeBatsman: 1,
+      displayExtras: false,
+      extrasAmount: 0
     }
   },
 
 
   methods: {
     confirmPlayingXi() {
-
       this.moduleNo++
       this.cpxi = false
       this.moduleName = 'Start Scoring'
       this.csxi = true
     },
     increaseScore(scoreValue) {
-      console.log(scoreValue);
 
-      const batScores = [1, 2, 3, 4, 5, 6]
+      const batScores = [0, 1, 2, 3, 4, 5, 6]
       if (batScores.includes(Number(scoreValue))) {
 
         this.score += Number(scoreValue)
+
+        this.calculateBatsmanScore(scoreValue);
         this.changeOvers();
 
       }
       else if (scoreValue == 'o') {
 
-        this.changeOvers();
         this.wickets++
+
+        this.changeOvers();
+        this.calculateBatsmanScore(0);
 
         if (Number(this.wickets) == 10) {
           this.cont = false
         }
 
       }
-      else if (scoreValue == 'w') {
-        this.score++
+      else if (scoreValue == 'w' || scoreValue == 'n' || scoreValue == 'l' || scoreValue == 'b') {
+        this.displayExtras = true
       }
 
       this.changeStrike(scoreValue);
@@ -71,10 +76,17 @@ const app = createApp({
         //change strike
         console.log('change strike');
       }
+    },
+    calculateBatsmanScore(scoreValue) {
+      console.log(scoreValue)
+      this.playingXi[this.wickets].runs += Number(scoreValue)
+      console.log(this.playingXi[this.wickets].runs)
+    },
+    calculateExtras() {
+      this.score = 1 + Number(this.score) + Number(this.extrasAmount)
+      console.log('wuhu' + Number(this.score))
+      this.displayExtras = false
     }
   }
-
-
-
 
 }).mount("#app")
